7d278414a571f705bb8212d955151d41903eb9a3
Mohit <mohitjangir17@gmail.com>
2025-04-27 22:55:57 +0530

    vercel config

--- server.js
+++ server.js
@@ -24,5 +24,8 @@ app.use('/api/client', clientRoutes);
 // Error Handler
 app.use(errorHandler);
 
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+export default app;
